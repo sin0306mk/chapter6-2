8c87634cf1043975b9dee47ebba6dc50c663505d
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-18 16:06:11 +0900

    三浦大輔を追加

--- chapter6-2/ViewController.swift
+++ chapter6-2/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myURL = NSURL(string: "http://sportiva.shueisha.co.jp/clm/baseball/entry_img/0610miura-001.jpg")
+        var myData = NSData(contentsOfURL: myURL!)
+        var myImage = UIImage(data: myData!)
+        
+        var imgPath = NSBundle.mainBundle().pathForResource("0610miura-001", ofType: "jpg")
+        
+        myImageView.image = myImage
+//        myImageView.image = UIImage(contentsOfFile: imgPath!)
+        
     }
 
     override func didReceiveMemoryWarning() {
